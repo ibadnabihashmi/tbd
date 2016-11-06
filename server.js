40f46975ab580e778237b04d85939c7a9df90790
ibadnabihashmi <ibad.hashmi@yahoo.com>
2016-11-06 21:52:22 +0500

    ....

--- server.js
+++ server.js
@@ -25,7 +25,20 @@ var image             = require('./api/image');
 require('./config/passport');
 
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 
 mongoose.connect(process.env.MONGODB);
 mongoose.connection.on('error', function() {
@@ -35,6 +48,7 @@ mongoose.connection.on('error', function() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
+app.use(allowCrossDomain);
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
